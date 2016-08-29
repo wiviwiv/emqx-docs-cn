9148fcd40e640db677a0a1cf96cd190515f5ae62
zhangwh <zh.whong@gmail.com>
2016-08-29 22:11:14 +0800

    make_html.sh add merge

--- make_html.sh
+++ make_html.sh
@@ -2,6 +2,6 @@
 
 git checkout master
 git pull
-git checkout emqtt.com
+git checkout emqtt.com && git merge master
 source .venv/bin/activate && make html
 cp -r build/html/* ../emqtt.com/public/docs/v1/
